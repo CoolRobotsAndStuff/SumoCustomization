b0608e29e7ae938bb282d3157ded40bc5e0ed887
Alejandro de Ugarriza <aledeum.saf@gmail.com>
2021-07-15 15:31:55 -0300

    agregado todo

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CheckBoxComponent } from './component-checkbox/component-checkbox.component';
+import { SliderComponent } from './component-slider/component-slider.component';
+import { PositionRotationComponent } from './position-rot-inputs/position-rot.component';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import { HttpClientModule  } from '@angular/common/http';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CheckBoxComponent,
+    SliderComponent,
+    PositionRotationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    
+    MatSliderModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatSnackBarModule,
+    HttpClientModule 
+  ],
+  providers: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/component_costs.ts
@@ -0,0 +1,9 @@
+export const components = [
+    {
+        name: "Sensor de Color",
+        dictName: "colsensor",
+        defaultName: "colour_sensor",
+        cost: 100
+    }
+
+]
